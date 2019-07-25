3e8ade8e0f0ea15676892c7f7ee6e9c31c07449f
Shaheer Choudhury <shaheer.choudhury@gmail.com>
2019-07-25 15:06:11 -0400

    some commit

--- my_first_web_app/urls.py
+++ my_first_web_app/urls.py
@@ -14,12 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from random import randint
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.urls import path
 
 def home_page(request):
-    return HttpResponse('Hi, this is the homepage')
+    response = render(request, 'index.html')
+    return HttpResponse(response)
+
+def gallery(request):
+    random_number = randint(0, 100)
+    image_url = "https://picsum.photos/400/600/?image={}".format(random_number)
+    context = {'gallery_image': image_url}
+    response = render(request, 'gallery.html', context)
+    return HttpResponse(response)
 
 urlpatterns = [
-    path('home/', home_page)
+    path('home/', home_page),
+    path('portfolio/', gallery)
 ]
